f12b27e6f8ba6efd040150be732f6f77ea880e4e
Gary McDonald <mcdonagj@dukes.jmu.edu>
2018-11-11 13:43:56 -0500

    Add scoreboard.

--- framework/games/pong.js
+++ framework/games/pong.js
@@ -5,6 +5,9 @@ function gameMessageHandler(msg) { }
 
 var touchX = 0, touchY = 0, lastTouchX = 0, lastTouchY = 0;
 
+// Set ball speed.
+var xSpeed = 4;
+
 // Set ball height & width.
 var ballHeight = (windowHeight / 17.5), ballWidth = (windowHeight / 17.5);
 
@@ -14,17 +17,24 @@ var playerA_Height = (windowHeight / 4.5), playerB_Height = (windowHeight / 4.5)
 // Set player A, player B paddle sizes.
 var playerA_Width = 15, playerB_Width = 15;
 
-// X Position should stay fixed; Y Position changes based on input.
-var playerA_XPosition = 100, playerA_YPosition = 500;
+// Set parameters for the front of each player's paddle.
+var FRONT_OF_PLAYER_A = (playerA_XPosition + (playerA_Width / 2));
+var FRONT_OF_PLAYER_B = (playerB_XPosition - (playerB_Width / 2));
 
 // X Position should stay fixed; Y Position changes based on input.
-var playerB_XPosition = (windowWidth - 100), playerB_YPosition = 500;
+var playerA_XPosition = 100, playerA_YPosition = (windowHeight / 2);
+
+// X Position should stay fixed; Y Position changes based on input.
+var playerB_XPosition = (windowWidth - 100), playerB_YPosition = (windowHeight / 2);
 
 // Start the ball's position in the center of the screen.
 var ball_XPosition = (windowWidth / 2), ball_YPosition = (windowHeight / 2);
 
 // Store player identifiers and corresponding y coordinates.
 var player_IDs = [0, 0], playerID_Y = [0, 0];
+
+// Store the player scores.
+var player_Scores = [0, 0];
 
 function touchMove(x, y, id) {
     var i;
@@ -74,16 +84,57 @@ function createBall() {
     ellipse(ball_XPosition, ball_YPosition, ballWidth, ballHeight);
 }
 
-function createArena() {
-    // TODO: Create bounds for where the ball is in play. 
-    // rect(x, y, width, height)
-    rect(0, 0, windowWidth, windowHeight);
+function createScoreboard() {
+
+    fill(0, 0, 0);
+    
+    rect((windowWidth/2) - 125, windowHeight - 100, 250, 250);
+
+    fill(255, 255, 255);
+    textSize(25);
+    text(player_Scores[0], ((windowWidth+100)) / 2, 700);
+
+    fill(255, 255, 255);
+    textSize(25);
+    text(player_Scores[1], ((windowWidth-100)) / 2, 700);
+    
+}
+
+function resetBoard(i) {
+    Console.log("Player " + i + " wins!")
+}
+
+function changeDirection() {
+    xSpeed = xSpeed * -1;
 }
 
 function draw() {
     background(51);
-    createArena();
     createPlayerA();
     createPlayerB();
+    createScoreboard();
     createBall();
+    frameRate(120);
+    ball_XPosition = ball_XPosition + xSpeed;
+
+    if (ball_XPosition > windowWidth) {
+        ball_XPosition = (windowWidth / 2);
+        player_Scores[0] = player_Scores[0] + 1;
+    }
+
+    if (ball_XPosition < 0) {
+        ball_XPosition = (windowWidth / 2);
+        player_Scores[1] = player_Scores[1] + 1;
+    }
+
+    if (ball_YPosition == playerID_Y[0]) {
+        //xSpeed = 4;
+        changeDirection();
+    }
+
+    if (ball_YPosition == playerID_Y[1]) {
+        //xSpeed = -4;
+        //changeDirection();
+    }
+    
 }
